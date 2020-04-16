bf66c7bdfdbc60045e9458e144af8247ca8a6df6
bergeouss <bergeouss@gmail.com>
2020-04-16 19:43:27 +0200

    First commit

---
+++ lib/crypto_scraper.rb
@@ -0,0 +1,29 @@
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+def getcryptocurrency
+  rates = []
+  crypnames = []
+  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all"))  
+
+    page.xpath('/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each do |tr|
+    crypnames << tr.text
+  end 
+
+    page.xpath('/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/a').each do |tr|
+      rates << tr.text.gsub(/[^\d\.]/, '').to_f
+  end
+
+  begin
+    list = []
+  market = Hash.new
+  market = Hash[crypnames.zip(rates)]
+    puts market.count
+    list = market.keys.map { |key| r = {}; r[key] = hash[key]  }
+    puts list
+  end
+
+end
+getcryptocurrency
