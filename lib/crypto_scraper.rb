40cbed08044edf9d7bc8585dce956f67de64aac4
bergeouss <bergeouss@gmail.com>
2020-04-16 21:12:44 +0200

    crypto_scraper ok

--- lib/crypto_scraper.rb
+++ lib/crypto_scraper.rb
@@ -8,21 +8,21 @@ def getcryptocurrency
   crypnames = []
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all"))  
 
-    page.xpath('/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each do |tr|
+  page.xpath('/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each do |tr|
     crypnames << tr.text
   end 
 
     page.xpath('/html/body/div/div/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/a').each do |tr|
-      rates << tr.text.gsub(/[^\d\.]/, '').to_f
+    rates << tr.text.gsub(/[^\d\.]/, '').to_f
   end
 
   begin
     list = []
-  market = Hash.new
-  market = Hash[crypnames.zip(rates)]
-    puts market.count
-    list = market.keys.map { |key| r = {}; r[key] = hash[key]  }
+    market = Hash.new
+    market = Hash[crypnames.zip(rates)]
+    list = market.map { |k, v| { k => v } }
     puts list
+    puts "This list is an #{list.class} of #{market.count} crypto-currencies' rates in $"
   end
 
 end
